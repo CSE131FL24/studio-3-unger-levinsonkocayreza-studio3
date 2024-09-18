06e5c35fb19ae47cfe5bb691b6f89e5c46fb1d30
kiraul <kiraul@localhost>
2024-09-18 14:16:39 -0500

    our studio work

---
+++ src/studio3/Studio3.java
@@ -0,0 +1,23 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Studio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println ("What number do you want to sieve through?");
+		int n = in.nextInt();
+		int[] numbers = new int [n];
+		boolean [] isPrime = new boolean [n];
+		for (int i=0; i<n; i++) {
+			isPrime [i] = true; }
+		for (int k=2; k<=n; k++) {
+			if (isPrime[k] =true);
+			
+			
+	}
+		for (int j=2; j<n; j++){
+			
+}}}
